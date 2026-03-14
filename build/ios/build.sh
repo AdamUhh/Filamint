acac95d669ef516bd5cecfb370bb11baba351bc4
AdamUhh <adamuhh.dev@gmail.com>
2026-03-14 23:05:31 +0400

    chore(version): updated app meta to v0.2.1

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="filamint"
 BUNDLE_ID="com.adamuhh.filamint"
-VERSION="0.2.0"
-BUILD_NUMBER="0.2.0"
+VERSION="0.2.1"
+BUILD_NUMBER="0.2.1"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
